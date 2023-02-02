8407d49a1a8005d81b5714499bab1b71a44ef413
Eva Kerrigan <evakerrigan@gmail.com>
2023-02-02 20:51:32 +0300

    refactor: typeguard change to as at ts in index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,19 +4,15 @@ import "./index.scss";
 import App from './App';
 import { HashRouter } from "react-router-dom";
 
-const rootElement: HTMLElement | null = document.getElementById("root");
+const rootElement = document.getElementById("root") as HTMLElement;
 
-if (rootElement instanceof HTMLElement) {
+const root = ReactDOM.createRoot(rootElement);
 
-  const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 
-  root.render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>
-
-  );
-
-}
+);
