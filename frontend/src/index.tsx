ff821cf7ff6eb27687ddf0866e5502f1e459deed
osipik.a <osipik.av@yandex.by>
2023-02-03 23:30:18 +0300

    refactor: formatting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
-
+  </React.StrictMode>,
 );
