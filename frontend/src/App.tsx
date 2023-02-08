a90c3828440bffa81903a892e8ea09bfba343984
osipik.a <osipik.av@yandex.by>
2023-02-08 20:32:42 +0300

    fix: routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
 import './App.scss';
 import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
 import { Header } from './components/header/Header';
-import { Main } from './components/main/Main';
 import { Footer } from './components/footer/Footer';
+import { Messages } from './components/pages/messages/Messages';
+import { Map } from './components/pages/map/Map';
+import { NotFound } from './components/pages/notFound/NotFound';
+import { Profile } from './components/pages/profile/Profile';
+
 
 function App() {
   return (
     <section className='App'>
       <Header />
-      <Main />
+      <main className='main'>
+       <Routes>
+        <Route path='/' element={<Map/>} />
+        <Route path='/messages' element={<Messages/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/*' element={<NotFound/>} />
+      </Routes>
+    </main>
       <Footer />
     </section>
   );
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { UserOutlined } from '@ant-design/icons';
 import { Row, Col, Avatar, Space } from 'antd';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 const userName = 'User';
@@ -8,12 +9,21 @@ export function Header() {
   return (
     <header className='header'>
       <Row align='middle'>
-        <Col span={8}> </Col>
+        <Col span={8}>
+        <nav className='nav'>
+          <Space >
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/messages'>Messages</NavLink>
+          </Space>
+        </nav>
+        </Col>
         <Col span={8}>RS CLUB</Col>
         <Col span={8}>
           <Space>
             <span> Hello, {userName}! </span>
+            <NavLink to='/profile'>
             <Avatar className='avatar' size={42} icon={<UserOutlined />} />
+            </NavLink>
           </Space>
         </Col>
       </Row>
