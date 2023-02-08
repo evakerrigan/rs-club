2af4df4ad32fe97b87f5b361b0dcc0b163f98690
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-08 13:56:35 +0500

    feat: first version map

---
+++ frontend/src/components/MyMap/MyMap.tsx
@@ -0,0 +1,43 @@
+import { YMaps, Map, Placemark, Clusterer } from '@pbe/react-yandex-maps';
+import './Map.scss';
+
+export function MyMap() {
+  /* mock coordinates for map */
+  const points = [
+    [55.733685, 37.588264],
+    [59.9386, 30.3141],
+    [57.1522, 65.5272],
+    [53.9, 27.57],
+    [56, 37.8],
+    [60, 30.3141],
+    [57.0522, 65.0272],
+    [54, 27.7],
+  ];
+
+  return (
+    <div className='main map-container'>
+      <div className='map-filters'>Filters</div>
+      <YMaps>
+        <Map
+          defaultState={{
+            center: [55.751574, 37.573856],
+            zoom: 5,
+          }}
+          className='map'
+        >
+          <Clusterer
+            options={{
+              preset: 'islands#invertedVioletClusterIcons',
+              groupByCoordinates: false,
+            }}
+          >
+            {points.map((coordinates, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Placemark key={index} geometry={coordinates} />
+            ))}
+          </Clusterer>
+        </Map>
+      </YMaps>
+    </div>
+  );
+}
