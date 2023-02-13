90dc6d8fcec640ad1382b9fb73a9a69235d23050
Eva Kerrigan <evakerrigan@gmail.com>
2023-02-14 01:32:40 +0300

    feat: start axios

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Row, Col, Avatar, Space } from 'antd';
+import { Row, Col, Avatar, Space, Button } from 'antd';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 import './Header.scss';
 
 const userName = 'User';
 
 export function Header() {
+
+  const instance = axios.create({
+    baseURL: 'http://localhost:8000',
+    withCredentials: true,
+  })
+
+  const onClick = () => axios.get('auth/callback',
+    // {
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    //   }
+    // }
+  )
   return (
     <header className='header'>
       <Row align='middle'>
@@ -18,6 +33,7 @@ export function Header() {
           <NavLink to='/'>RS CLUB</NavLink>
         </Col>
         <Col span={8}>
+          <Button onClick={onClick}>Reg Github</Button>
           <Space>
             <span> Hello, {userName}! </span>
             <NavLink to='/profile'>
