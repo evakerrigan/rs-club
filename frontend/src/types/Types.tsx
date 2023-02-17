176eecf6112737b48463b85d353faa36d6cbda77
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-17 22:07:43 +0500

    fix: update interface user

--- frontend/src/types/Types.tsx
+++ frontend/src/types/Types.tsx
@@ -1,12 +1,39 @@
-export interface IUser {
+/* export interface IUser {
   name: string;
   gender: 'male' | 'female';
   city: string;
   cources: string[];
   technologies: string[];
 }
-
+ */
 export interface IOption {
   value: string;
   key: string;
+}
+
+export interface FiltersData {
+  interests: string[];
+  technologies: string[];
+  cources: string[];
+}
+export interface Preference {
+  value: string;
+  category: string;
+  isActive: boolean;
+}
+export interface IUser {
+  githubName: string;
+  firstName: string;
+  lastName: string;
+  gender: 'male' | 'female';
+  profilePicture?: string;
+  address?: string;
+  location?: [number, number];
+  role: 'admin' | 'user';
+  signupDate: Date;
+  lastActivity: Date;
+  status: 'active' | 'inactive';
+  preferences: Preference[];
+  technology: string[];
+  courses: string[];
 }
