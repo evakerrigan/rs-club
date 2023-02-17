ae45d9219c92eb8a41c433b69140122f557dd504
osipik.a <osipik.av@yandex.by>
2023-02-18 02:07:17 +0300

    feat: finish layout

--- frontend/src/components/Messages/Messages.tsx
+++ frontend/src/components/Messages/Messages.tsx
@@ -1,78 +1,90 @@
 import './Messages.scss';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import React from 'react';
-import { Avatar, Button, Form, Input } from 'antd';
+import { Button, Form } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
+// import { RenderMessage } from '../Message/Message';
 
-function Message() {
+const onFinish = (value: string) => {
+  // eslint-disable-next-line no-console
+  console.log(value);
+};
+
+function RenderChat() {
+  return (
+    <div className='chat-avatar'>
+      <img alt='avatar' src='https://game-assets.swgoh.gg/tex.charui_admiralraddus.png' />
+    </div>
+  );
+}
+
+function RenderMessage() {
   // console.log('object :>> ', avatar, text, date);
   return (
     <>
-      {/* <div className='message__item input-message output-message'> */}
-      <div className='message__avatar'>
+      {/* <div className='message-item input-message output-message'> */}
+      <div className='message-avatar'>
         <img alt='avatar' src='https://game-assets.swgoh.gg/tex.charui_admiralraddus.png' />
       </div>
-
-      <div className='message__content'>
-        {/* <div className='message__bubble'> */}
-        <p className='message__text'> asdawd awdwd awdwdadawdawdasdawd w wda aw awdaw dd</p>
-        {/* </div> */}
-        <p className='message__date'> Apr 21 2023 21:32:07</p>
+      <div className='message-content'>
+        <p className='message-text'> asdawd awdwd awdwdadawdawdasdawd w wda aw awdaw dd</p>
+        <p className='message-date'> Apr 21 2023 21:32:07</p>
       </div>
-      {/* </div> */}
     </>
   );
 }
 
-// Message.propTypes = {
-//   avatar: PropTypes.string,
-//   text: PropTypes.string,
-//   date: PropTypes.string,
-//   // user: PropTypes.object,
-// };
 export function Messages() {
   return (
     <main className='main messages'>
-      <section className='interlocutors'>interlocutors</section>
+      <section className='chats'>
+        <RenderChat />
+        <RenderChat />
+      </section>
       <section className='messages-container'>
-        <div>
-          <div className='message__item input-message '>
-            <Message
+        <div className='message-history'>
+          <div className='message-item input-message '>
+            <RenderMessage
             // avatar='https://game-assets.swgoh.gg/tex.charui_admiralraddus.png'
             // text='asdasdasd asd awdawdasd awd s'
             // date='Sun Apr 21 2023 21:32:07'
             />
           </div>
-          <div className='message__item output-message'>
-            <Message
-
-            // avatar='https://game-assets.swgoh.gg/tex.charui_admiralraddus.png'
-            // text='asdasdasd asd awdawdasd awd s'
-            // date='Sun Apr 21 2023 21:32:07'
-            />
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item input-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item input-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
+          </div>
+          <div className='message-item output-message'>
+            <RenderMessage />
           </div>
         </div>
-        <div className='message-input'>
-          <Form>
-            <Form.Item name={['user', 'introduction']} label='Introduction'>
-              <Input.TextArea />
+        <div className='input-form'>
+          <Form onFinish={onFinish}>
+            <Button htmlType='submit'>Submit</Button>
+            <Form.Item name={['message']}>
+              <TextArea style={{ resize: 'none' }} placeholder='Input message...' />
             </Form.Item>
-            <Button type='primary' htmlType='submit'>
-              Submit
-            </Button>
           </Form>
         </div>
       </section>
     </main>
   );
 }
-
-// Message.defaultProps = {
-//   user: {},
-// };
-
-// Message.propTypes = {
-//   avatar: 'https://game-assets.swgoh.gg/tex.charui_admiralraddus.png',
-//   text: 'asdasdasd asd awdawdasd awd s',
-//   date: 'Sun Apr 21 2023 21:32:07',
-// };
