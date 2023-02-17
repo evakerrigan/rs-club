ff90c0a23338e15f50e599f62f919af3b4c96aa0
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-17 22:15:14 +0500

    feat: add response from backend

--- frontend/src/components/MyMap/MyMap.tsx
+++ frontend/src/components/MyMap/MyMap.tsx
@@ -1,24 +1,60 @@
 import { YMaps, Map, Placemark, Clusterer } from '@pbe/react-yandex-maps';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import {  IUser } from '../../types/Types';
 import { FiltersRender } from '../Filters/Filters';
 import './Map.scss';
 
 export function MyMap() {
-  /* mock coordinates for map */
-  const points = [
-    [55.733685, 37.588264],
-    [59.9386, 30.3141],
-    [57.1522, 65.5272],
-    [53.9, 27.57],
-    [56, 37.8],
-    [60, 30.3141],
-    [57.0522, 65.0272],
-    [54, 27.7],
-  ];
+  const [users, setUsers] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const stack = searchParams.get('stack');
+  const pref = searchParams.get('pref');
+  const cource = searchParams.get('cources');
+
+  const getCoordinates = () =>
+    users.map((item: IUser) => item.location).filter((item) => item?.length);
+  const resetParamsAndLocalStorage = () => {
+    localStorage.clear()
+    setSearchParams({})
+  }
+  const onFinish = () => {
+    const createPrefParams = JSON.parse(localStorage.getItem('pref') || '[]');
+    const createStackParams = JSON.parse(localStorage.getItem('stack') || '[]');
+    const createCourcesParams = JSON.parse(localStorage.getItem('cources') || '[]');
+    const query = {
+      stack: createStackParams,
+      pref: createPrefParams,
+      cources: createCourcesParams,
+    };
+    setSearchParams(Object.fromEntries(Object.entries(query).filter((e) => e[1])));
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const BASE_URL = 'http://localhost:8000/api/users?';
+        const query: string[] = [];
+        if (stack) query.push(`stack=${stack}`);
+        if (pref) query.push(`pref=${pref}`);
+        if (cource) query.push(`cources=${pref}`);
+
+        const response = await fetch(BASE_URL + query.join('&'));
+        const data = await response.json();
+        setUsers(data.items);
+      } catch (error) {
+        console.log(`Oops, the server data was not loaded! ${error}`);
+      }
+    };
+    getUsers();
+  }, [cource, pref, stack]);
 
   return (
     <div className='map-container'>
-             <FiltersRender/>
-           <YMaps>
+      <FiltersRender onFinish={onFinish} resetParamsAndLocalStorage={resetParamsAndLocalStorage} />
+      <YMaps>
         <Map
           defaultState={{
             center: [55.751574, 37.573856],
@@ -32,7 +68,7 @@ export function MyMap() {
               groupByCoordinates: false,
             }}
           >
-            {points.map((coordinates, index) => (
+            {getCoordinates().map((coordinates, index) => (
               // eslint-disable-next-line react/no-array-index-key
               <Placemark key={index} geometry={coordinates} />
             ))}
