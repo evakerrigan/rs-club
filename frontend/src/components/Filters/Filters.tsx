c1b5628106870ceda4c97cad3d8809a3758ba397
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-17 22:13:22 +0500

    feat: add filters & query params & save filters in localstorage

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,73 +1,85 @@
-import { Button, Checkbox, Form, } from 'antd';
-import {  CheckboxValueType } from 'antd/es/checkbox/Group';
-// import { generatePath } from 'react-router-dom';
+import { Button, Checkbox, Form } from 'antd';
+import { CheckboxValueType } from 'antd/es/checkbox/Group';
 
 import './Filters.scss';
-import { courcesList, interestsList, technologiesList} from '../Constants/Constants';
-import { IOption } from '../../types/Types';
+import { courcesList, interestsList, technologiesList } from '../Constants/Constants';
+import { FiltersData, IOption } from '../../types/Types';
 
-// let filter = {
-//   interests:[],
-//   technologies:[],
-//   cources:[],
-// };
+const onChange = (checkedValues: CheckboxValueType[], query: string) => {
+  const findItem = JSON.parse(localStorage.getItem(query) || '[]');
 
-const onChange = (checkedValues: CheckboxValueType[] ) => {
-    console.log('checked = ', checkedValues);
-// const item ='in my city erwe sdasd ';
-    // console.log('object :>> ', generatePath('/users/:id', {id:String(checkedValues)}));
-    // console.log(CreateKey(item));
-    // http://localhost:8000/api/users?pref=${params}
-  };
+  if (!findItem.includes(checkedValues.at(-1))) {
+    localStorage.setItem(query, JSON.stringify(checkedValues));
+  } else {
+    localStorage.setItem(query, JSON.stringify(checkedValues.slice(0, checkedValues.length)));
+  }
+};
 
-  const onFinish = (values: string[][]=[[],[],[]]) => {
-      console.log(values);
-  };
+const ChangeStyle = (event: React.MouseEvent<HTMLElement>): void => {
+  event.currentTarget.closest('.checkbox-item')?.classList.toggle('checked');
+};
 
-const ChangeStyle = (event: React.MouseEvent<HTMLElement>): void=> {
-    event.currentTarget.closest('.checkbox-item')?.classList.toggle('checked');
-    };
+const isCheckedFromLocalStorage = (str: string, key: string) => {
+  const array = JSON.parse(localStorage.getItem(str) || '[]');
+  return array.includes(key);
+};
 
-function CreateCheckbox(list: IOption[]=[]) {
+function CreateCheckbox(query: string, list: IOption[] = []) {
   return (
-       <div className='filter-section'>
-         {list.map((item) => (
-          <div className='checkbox-item' key={item.key} >
-             <Checkbox  value={item.value} onClick={ChangeStyle}>{item.value}</Checkbox>
-          </div>
-      ))
-      }
-      </div>
+    <div className='filter-section'>
+      {list.map((item) => (
+        <div
+          className={`checkbox-item ${isCheckedFromLocalStorage(query, item.key) && 'checked'}`}
+          key={item.key}
+        >
+          <Checkbox
+            value={item.key}
+            onClick={ChangeStyle}
+            checked={isCheckedFromLocalStorage(query, item.key)}
+          >
+            {item.value}
+          </Checkbox>
+        </div>
+      ))}
+    </div>
   );
 }
 
-export function FiltersRender() {
+export function FiltersRender({
+  onFinish,
+  resetParamsAndLocalStorage,
+}: {
+  onFinish: (value: FiltersData) => void;
+  resetParamsAndLocalStorage: () => void;
+}) {
   return (
-    <Form className='filters'
-    onFinish={onFinish}
-    >
+    <Form className='filters' onFinish={onFinish}>
       <Form.Item name='interests'>
-    <Checkbox.Group className='filter-item' onChange={onChange}>
-      <h2 className='filter-title'>Interests</h2>
-      {CreateCheckbox(interestsList)}
-    </Checkbox.Group>
-    </Form.Item>
-    <Form.Item name='technologies' >
-    <Checkbox.Group className='filter-item' onChange={onChange}>
-        <h2 className='filter-title'>Technologies</h2>
-        {CreateCheckbox(technologiesList)}
-    </Checkbox.Group>
-    </Form.Item>
-    <Form.Item name='cources'>
-    <Checkbox.Group className='filter-item' onChange={onChange}>
-        <h2 className='filter-title'>Cources</h2>
-        {CreateCheckbox(courcesList)}
-    </Checkbox.Group>
-    </Form.Item>
-    <Button type='primary' htmlType='submit'>
-            Confirm
-          </Button>
-      </Form>
+        <Checkbox.Group className='filter-item' onChange={(data) => onChange(data, 'pref')}>
+          <h2 className='filter-title'>Interests</h2>
+          {CreateCheckbox('pref', interestsList)}
+        </Checkbox.Group>
+      </Form.Item>
+      <Form.Item name='technologies'>
+        <Checkbox.Group className='filter-item' onChange={(data) => onChange(data, 'stack')}>
+          <h2 className='filter-title'>Technologies</h2>
+          {CreateCheckbox('stack', technologiesList)}
+        </Checkbox.Group>
+      </Form.Item>
+      <Form.Item name='cources'>
+        <Checkbox.Group className='filter-item' onChange={(data) => onChange(data, 'cources')}>
+          <h2 className='filter-title'>Cources</h2>
+          {CreateCheckbox('cources', courcesList)}
+        </Checkbox.Group>
+      </Form.Item>
+      <div className='filters__buttons'>
+        <Button type='primary' htmlType='submit' size='large' block>
+          Confirm
+        </Button>
+        <Button type='primary' onClick={resetParamsAndLocalStorage} size='large' block>
+          Reset
+        </Button>
+      </div>
+    </Form>
   );
 }
-
