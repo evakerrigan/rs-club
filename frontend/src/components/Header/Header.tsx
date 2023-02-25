0f0b25a8108c8ca0e8bd51f51f0dd98cfd61ac01
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-25 15:43:15 +0500

    feat: update header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,25 +1,40 @@
 import { UserOutlined } from '@ant-design/icons';
 import { Row, Col, Avatar, Space, Button } from 'antd';
 import { NavLink } from 'react-router-dom';
+import { IAuthorizationResponse } from '../../types/Types';
+import { getCookie } from '../../utils/getCookie';
+import { BASE_URL } from '../Constants/Constants';
 import './Header.scss';
-
-const userName = 'User';
-
-const onClickReg = () => {
-  if (process.env.NODE_ENV === 'development') {
-    window.location.href = 'http://localhost:8000/api/auth/';
-  } else {
-    window.location.href = '/api/auth/'
-  }
-}
 
 type Props = {
   isAuthenticated: boolean;
-}
+  userName: string;
+  userAvatar: string;
+};
 
-export function Header({isAuthenticated}:Props) {
+export function Header({ isAuthenticated, userName = 'User', userAvatar }: Props) {
+  const onClickReg = () => {
+    const rsAccessToken = getCookie('rsAccessToken');
+    const AUTH_VALIDATE = `${BASE_URL}/auth/validate`;
+    const getUser = async () => {
+      try {
+        const response = await fetch(AUTH_VALIDATE, {
+          headers: {
+            Authorization: `Bearer ${rsAccessToken}`,
+          },
+        });
+        /* если 200 то все ок, если 401, то реконект на логин */
+        const data: IAuthorizationResponse = await response.json();
+        if (data.statusCode === 401) {
+          window.location.href = `${BASE_URL}/auth/`;
+        }
+      } catch (error) {
+        console.error('Authorization failed!')
+      }
 
-  console.log('header isAuthenticated =', isAuthenticated);
+    };
+    getUser();
+  };
 
   return (
     <header className='header'>
@@ -33,18 +48,23 @@ export function Header({isAuthenticated}:Props) {
           <NavLink to='/'>RS CLUB</NavLink>
         </Col>
         <Col span={8}>
-          {
-            isAuthenticated === false ?
-              <Button onClick={onClickReg}>Reg Github</Button>
-              : <Space>
-                <span> Hello, {userName}! </span>
-                <NavLink to='/profile'>
-                  <Avatar className='avatar' size={42} icon={<UserOutlined />} />
-                </NavLink>
-              </Space>
-          }
+          {isAuthenticated === false ? (
+            <Button onClick={onClickReg}>Reg Github</Button>
+          ) : (
+            <Space>
+              <span> Hello, {userName}! </span>
+              <NavLink to='/profile'>
+                <Avatar
+                  className='avatar'
+                  size={42}
+                  icon={<UserOutlined />}
+                  src={userAvatar || ''}
+                />
+              </NavLink>
+            </Space>
+          )}
         </Col>
       </Row>
-    </header >
+    </header>
   );
 }
