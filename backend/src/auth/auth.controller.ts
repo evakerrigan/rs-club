8e6650cf38caeef3d4a35b8e04d2eedd4af8a046
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-02-25 15:37:48 +0500

    feat: update auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, Response, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
 import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
@@ -12,43 +13,48 @@ export class AuthController {
     private readonly usersService: UsersService,
   ) {}
 
+  @Get('validate')
+  @UseGuards(JwtAuthGuard)
+  async valideToken() {
+    return { statusCode: 200, message: 'Authorized' };
+  }
+
   @Get()
   @UseGuards(AuthGuard('github'))
   async login() {
     //
-  }
-  
-  async validateUser(username: string, rsAccessToken: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.rsAccessToken === rsAccessToken) {
-      const { rsAccessToken, ...result } = user;
-      return result;
-    }
-    return null;
   }
 
   @Get('callback')
   @UseGuards(AuthGuard('github'))
   async authCallback(@Req() req, @Response() res) {
     const user = req.user;
-    const userName = user.username;
-    const avatarUrl = user.photos[0].value 
+    const githubName: string = user.username;
+    const avatarUrl = user.photos[0].value;
+    const findUser = await this.usersService.findByCond({ githubName });
+    let id;
+    if (!findUser) {
+      id = await this.usersService.create({
+        githubName,
+        profilePicture: avatarUrl,
+      });
+    }
     const payload = { sub: user.id, username: user.username };
-
     const rsAccessToken = this.jwtService.sign(payload);
-
-    this.usersService.create({
-      githubName: userName,
-      profilePicture: avatarUrl,
-      rsAccessToken,
-    });
+    /* 
+      ищу юзера в бд, если нет, то создаем, а если есть, обновляю токен
+    */
+    const userID = id ? String(id).valueOf() : String(findUser?._id).valueOf();
 
     res.cookie('rsAccessToken', rsAccessToken, {
       expires: new Date(new Date().getTime() + 30 * 1000),
       sameSite: 'strict',
       httpOnly: false,
     });
-    res.cookie('userName', userName, {
+    res.cookie('userName', githubName, {
+      expires: new Date(new Date().getTime() + 30 * 1000),
+    });
+    res.cookie('userId', userID, {
       expires: new Date(new Date().getTime() + 30 * 1000),
     });
 
